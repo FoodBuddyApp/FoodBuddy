5c002c174fe3a7741df6677f6d20cbac6419e07e
Scott Rizzo <jkarizzo345@yahoo.com>
2018-04-28 23:00:20 -0700

    added RecipeDetail

--- scripts/components/RecipeDetail/RecipeDetail.jsx
+++ scripts/components/RecipeDetail/RecipeDetail.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './RecipeDetail.css';
-
 import { Image, Button } from 'react-bootstrap';
 
 export default class RecipeDetail extends Component {
@@ -8,13 +7,36 @@ export default class RecipeDetail extends Component {
       super(props);
 
       this.state = {};
+      
    }
-
    render() {
       return (
-         <div>
+         <div className="centered-div">
             <h1>{this.props.title}</h1>
+            <Image src={this.props.image} />
+            <div className="cell center">
+               <h2>Ingredients</h2>
+               {this.props.missedIngredients.map((user) => {
+                  return <div key={user.id}><h4>{user.originalString}</h4></div>;
+               })}
+               {this.props.usedIngredientCount > 0 &&
+                  this.props.usedIngredients.map((user, i) => {
+                     return <div key={i}><h4>{user.originalString}</h4></div>;
+                  }) 
+               }
+            </div>
+               {this.props.RecipeDetail.analyzedInstructions.length > 0 &&
+                  <div className="cell left">
+                     <h2 className="center">Cooking Instructions</h2>
+                     {this.props.RecipeDetail.analyzedInstructions[0].steps.map((user) => {
+                        return <div key={user.number}><h4>Step {user.number}: {user.step}</h4></div>;
+                     })}
+                  </div>
+               }
+         
+            
          </div>
       );
    }
 }
+
