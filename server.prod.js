41f164d6ab6f321dc93ec806a33616c59dd550e8
OrangeJewc <samnayerman@gmail.com>
2018-04-30 21:34:19 -0700

    PLEASE FUCKING DEPLOY

--- server.prod.js
+++ server.prod.js
@@ -1,15 +1,28 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config.production');
+
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/static'));
 
-// views is directory for all template files
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
-});
+   response.sendFile(__dirname + '/index.html');
+ });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+
+app.use(function(req, res) {
+   res.header("Content-Type", "application/javascrip");
+})
+
+new WebpackDevServer(webpack(config), {
+   publicPath: config.output.publicPath,
+   hot: true,
+   historyApiFallback: true
+ }).listen(process.env.PORT || 5000, 'localhost', (err) => {
+   if (err) {
+     console.log(err);
+   }
+   console.log('Listening at localhost:5000');
+ });
