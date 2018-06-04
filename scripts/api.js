4232388b4a74f92513c860a90d7f6ad230b2ec1f
OrangeJewc <samnayerman@gmail.com>
2018-06-04 10:53:28 -0700

    change backend

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,5 @@
-// const baseURL = "https://nameless-hollows-31951.herokuapp.com/";
-const baseURL = "http://localhost:3001/";
+const baseURL = "https://nameless-hollows-31951.herokuapp.com/";
+// const baseURL = "http://localhost:3001/";
 const headers = new Headers();
 var cookie;
 
